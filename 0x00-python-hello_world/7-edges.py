a1ef952d6d2b4f87be875fca79abe9cb15cd27f6
dancan-agweli <agwelidancan32@gmail.com>
2023-05-08 13:55:08 -0700

    python

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:0+3]
+word_first_3 = word[:5-2]
 word_last_2 = word[-2:]
-middle_word = word[1:-2+1]
+middle_word = word[1:-1]
 print("the beginning 3 letters: {}".format(word_first_3))
 print("Last 2: {}".format(word_last_2))
-print("Middle letter: {}".format(middle_word))
+print("Middle word: {}".format(middle_word))
