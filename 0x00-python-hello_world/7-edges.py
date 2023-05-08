c950a88e4993a15d2d9b6173578b515febac75e1
dancan-agweli <agwelidancan32@gmail.com>
2023-05-08 13:51:04 -0700

    my python codes

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:0+3]
+word_last_2 = word[-2:]
+middle_word = word[1:-2+1]
+print("the beginning 3 letters: {}".format(word_first_3))
+print("Last 2: {}".format(word_last_2))
+print("Middle letter: {}".format(middle_word))
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[40-1:69-2] + str[110-3:111+1] + str[:8-2]
+print(str)
