c266728038321699b717642ad9461ba46dfff290
dancan-agweli <agwelidancan32@gmail.com>
2023-05-17 13:28:56 -0700

    python exece

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if (a_dictionary is None or len(a_dictionary) == 0):
         return None
-
-    a = list(a_dictionary.keys())[0]
-    b = a_dictionary[ret]
-    for m, n in a_dictionary.items():
-        if n > b:
-            b = n
-            a = m
-    return (a)
+    return max(a_dictionary, key=a_dictionary.get)
