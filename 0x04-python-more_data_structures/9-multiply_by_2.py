4aa532f8ba1c1e0ddecedf8154d29791910245ce
dancan-agweli <agwelidancan32@gmail.com>
2023-05-17 13:26:49 -0700

    python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    x = a_dictionary.copy()
-    for m, n in x.items():
-        x[m] = n * 2
-
-    return x
+    fresh = a_dictionary.copy()
+    for y, x in list(fresh.items()):
+        fresh[y] = x * 2
+    return fresh
