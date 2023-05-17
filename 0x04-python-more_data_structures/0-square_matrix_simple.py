eab1f70b1d98a1398292f5994510a54e5405061c
dancan-agweli <agwelidancan32@gmail.com>
2023-05-17 11:34:21 -0700

    python 3

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    data = ''
+
+    if len(matrix) > 0:
+        for u in matrix[:]:
+            data.append(list(map(lambda x: x ** 2, u)))
+    return data
