ea2e388cc48d3447138395e5d8988e5db19622ad
dancan-agweli <agwelidancan32@gmail.com>
2023-05-29 12:30:16 -0700

    python

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-#print the num
+
     for k in range(x):
         try:
             if type(my_list[k]) is int and num != x:
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    #return name error if not declared
     raise NameError(message)
