9652180bbfd3072ba2c3bae99f511d6eb039e84a
dancan-agweli <agwelidancan32@gmail.com>
2023-05-30 11:32:17 -0700

    python classess

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Declare class Square."""
+
+
+class Square:
+    """the squares."""
+
+    def __init__(self, size):
+        """Initialize a the  Square.
+
+        Args:
+            size (int): The size of the created square.
+        """
+        self.__size = size
