0f1d285338c450a4c40cd227c1d8727d4699921c
dancan-agweli <agwelidanan32@gmail.com>
2023-08-14 09:25:03 -0700

    javascripts warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
-function factorial(n) {
-  return (n === 0 || isNaN(n)) ? 1 : n * factorial(n - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const inputNumber = Number(process.argv[2]);
-
-if (isNaN(inputNumber) || process.argv[2] === undefined) {
-  console.log('Invalid input. Please provide a number.');
-} else {
-  console.log(`Factorial of ${inputNumber} is: ${factorial(inputNumber)}`);
-}
-
+console.log(factorial(Number(process.argv[2])));
