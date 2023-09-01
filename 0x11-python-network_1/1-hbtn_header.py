dbb5387f60a67dc4d350f0e80275ed6df917ed83
dancan-agweli <agwelidanan32@gmail.com>
2023-09-01 06:09:02 -0700

    python network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,20 +1,8 @@
-i#!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status."""
-import urllib.request
-import urllib.error
-
+#!/usr/bin/python3
+""" fetches yhe url"""
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("HTTP Error:", e.code)
-    except urllib.error.URLError as e:
-        print("URL Error:", e.reason)
-
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
+    with request.urlopen(req) as r:
+        print(r.headers.get('X-Request-Id'))
